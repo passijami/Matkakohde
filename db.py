a6d0a7cdeb66e446ecd71a1d6207567b3d5b724f
jamip <jamipassi.jp@gmail.com>
2025-03-22 18:10:34 +0200

    Add registration/login/logout

---
+++ db.py
@@ -0,0 +1,34 @@
+import sqlite3
+from flask import g
+
+def get_connection():
+    con = sqlite3.connect("database.db")
+    con.execute("PRAGMA foreign_keys = ON")
+    con.row_factory = sqlite3.Row
+    return con
+
+def execute(sql, params=[]):
+    con = get_connection()
+    try:
+        g.last_insert_id = result.lastrowid
+        result = con.execute(sql, params)
+        con.commit()
+        
+    except sqlite3.Error as e:
+        con.rollback()
+        raise e
+    finally:
+        con.close()
+
+def last_insert_id():
+    return getattr(g, 'last_insert_id', None)
+
+def query(sql, params=[]):
+    con = get_connection()
+    try:
+        result = con.execute(sql, params).fetchall()
+    except sqlite3.Error as e:
+        raise e
+    finally:
+        con.close()
+    return result
