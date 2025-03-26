5389b13e4aa078c5bcef3e987818c710d8b329b9
jamip <jamipassi.jp@gmail.com>
2025-03-26 21:55:21 +0200

    Add edit item feature

--- app.py
+++ app.py
@@ -35,6 +35,22 @@ def create_item():
 
     return redirect("/")
 
+@app.route("/edit_item/<int:item_id>")
+def edit_item(item_id):
+    item = items.get_item(item_id)
+    return render_template("edit_item.html", item=item)
+
+@app.route("/update_item", methods=["POST"])
+def update_item():
+    item_id = request.form["item_id"]
+    title = request.form["title"]
+    description = request.form["description"]
+    budget = request.form["budget"]
+
+    items.update_item(item_id, title, description, budget)
+
+    return redirect("/item/" + str(item_id))
+
 @app.route("/register")
 def register():
     return render_template("register.html")
@@ -83,4 +99,4 @@ def logout():
     return redirect("/")
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
--- items.py
+++ items.py
@@ -9,11 +9,20 @@ def get_items():
     return db.query(sql)
 
 def get_item(item_id):
-    sql = """SELECT items.title,
+    sql = """SELECT items.id,
+                    items.title,
                     items.description,
                     items.budget,
+                    user.id user_id,
                     users.username
              FROM items, users
              WHERE items.user_id = users.id AND
                    items.id = ?"""
     return db.query(sql, [item_id])[0]
+
+def update_item(item_id, title, description, budget):
+    sql = """UPDATE items SET title = ?,
+                              description = ?,
+                              budget = ?
+                          WHERE id = ?"""
+    db.execute(sql, [title, description, budget, item_id])
