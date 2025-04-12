3ecbcfc7011f0764d5a69dd9c80c9f6e955d7dec
jamip <155644993+passijami@users.noreply.github.com>
2025-04-12 22:42:37 +0300

    Create init.sql

---
+++ init.sql
@@ -0,0 +1,10 @@
+DELETE FROM classes;
+
+INSERT INTO classes (title, value) VALUES ('Osasto', 'maat')
+INSERT INTO classes (title, value) VALUES ('Osasto', 'kaupungit')
+INSERT INTO classes (title, value) VALUES ('Osasto', 'muut')
+
+INSERT INTO classes (title, value) VALUES ('Arvostelu', 'erinomainen')
+INSERT INTO classes (title, value) VALUES ('Arvostelu', 'hyvä')
+INSERT INTO classes (title, value) VALUES ('Arvostelu', 'tyydyttävä')
+INSERT INTO classes (title, value) VALUES ('Arvostelu', 'heikko')
