7e1033bc97280c3d6b1c7727827d8cdb45e9e865
jamip <155644993+passijami@users.noreply.github.com>
2025-05-03 20:45:29 +0300

    Update items.py
    improve db.py
    fix the errors
    add tools for images

--- items.py
+++ items.py
@@ -6,8 +6,8 @@ def get_all_classes():
     
     classes = {}
     for title, value in result:
-        classes[title] = []
-    for title, value in result:
+        if title not in classes:
+            classes[title] = []
         classes[title].append(value)
     
     return classes
@@ -24,12 +24,34 @@ def add_item(title, description, budget, user_id, classes):
     for title, value in classes:
         db.execute(sql, [item_id, title, value])
 
+def get_images(item_id):
+    sql = "SELECT id FROM images WHERE item_id = ?"
+    return db.query(sql, [item_id])
+
+def add_image(item_id, image):
+    sql = "INSERT INTO images (item_id, image) VALUES (?, ?)"
+    db.execute(sql, [item_id, image])
+
+def get_image(image_id):
+    sql = "SELECT image FROM images WHERE id = ?"
+    result = db.execute(sql, [image_id])
+    return result[0][0] if result else None
+
+def remove_image(item_id, image_id):
+    sql = "DELETE FROM images WHERE id = ? AND item_id = ?"
+    db.execute(sql, [image_id, item_id])
+
 def get_classes(item_id):
     sql = "SELECT title, value FROM item_classes WHERE item_id = ?"
     return db.query(sql, [item_id])
 
 def get_items():
-    sql = "SELECT id, title FROM items ORDER BY id DESC"
+    sql = """SELECT items.id, items.title, users.id user_id, users.username,
+                    COUNT(favorites.id) favorite_count
+             FROM items JOIN users ON items.user_id = users.id
+                        LEFT JOIN favorites ON items.id = favorites.item_id
+             GROUP BY items.id
+             ORDER BY items.id DESC"""
     return db.query(sql)
 
 def get_item(item_id):
@@ -59,20 +81,23 @@ def update_item(item_id, title, description, classes):
         db.execute(sql, [item_id, title, value])
 
 def remove_item(item_id):
+    sql = "DELETE FROM favorites WHERE item_id = ?"
+    db.execute(sql, [item_id])
+    sql = "DELETE FROM images WHERE item_id = ?"
+    db.execute(sql, [item_id])
     sql = "DELETE FROM item_classes WHERE item_id = ?"
     db.execute(sql, [item_id])
-
     sql = "DELETE FROM items WHERE id = ?"
     db.execute(sql, [item_id])
 
 def find_items(query):
     sql = """SELECT id, title
              FROM items
-             WHERE description LIKE ?
+             WHERE title LIKE ?
              ORDER BY id DESC"""
     like = "%" + query + "%"
     return db.query(sql, [like])
 
 def get_by_user(user_id):
     sql = "SELECT id, title FROM items WHERE user_id = ?"
-    return db.execute(sql, [user_id]).fetchall()
+    return db.execute(sql, [user_id])
