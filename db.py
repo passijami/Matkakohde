0bb791c251f79754097f3a7d58643ade98c3706d
jamip <155644993+passijami@users.noreply.github.com>
2025-05-03 20:37:16 +0300

    Update db.py
    fix the last_insert_id tool

--- db.py
+++ db.py
@@ -7,6 +7,7 @@ def get_connection():
     con.row_factory = sqlite3.Row
     return con
 
+
 def execute(sql, params=[]):
     con = get_connection()
     result = con.execute(sql, params)
@@ -15,9 +16,11 @@ def execute(sql, params=[]):
     con.close()
 
 def last_insert_id():
-    return g.last_insert_id    
-    
-def query(sql, params=[]):
+    return g.last_insert_id
+
+def query(sql, params=None):
+    if params is None:
+        params = []
     con = get_connection()
     result = con.execute(sql, params).fetchall()
     con.close()
